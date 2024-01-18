ec745d6e640592fa8b31c79fbf6a1d2434ec1ecb
thomaszhang2661 <hellothomaszhang@gmail.com>
2024-01-18 23:36:29 +0800

    Update enhance_para.py

--- enhance_para.py
+++ enhance_para.py
@@ -1,143 +1,366 @@
 import cv2
+import numpy as np
 import os
-import sys
-import shutil
-from enhance_utils import generate_blur, generate_blur_motion, generate_noisy, generate_photo_light, generate_photo_shadow, check_image_type
-from itertools import combinations
 import random
-from concurrent.futures import ProcessPoolExecutor
-import psutil
 import time
 
-def set_affinity(process_id, cpu_core):
-    try:
-        p = psutil.Process(process_id)
-        p.cpu_affinity([cpu_core])
-    except Exception as e:
-        print(f"Error setting affinity for process {process_id}: {e}")
 
-#input_image_path = '/root/autodl-tmp/training_data_gray/'
+# 模拟生成模糊图像
+def generate_blur(input_image, kernel_min=3, kernel_max=7):
+    if kernel_min < kernel_max:
+        kernel_min = kernel_max
 
-# enhance_mark 可以是 B M N L R 5个字母的任意组合
-# 总共可以生成的组合数：C(1/5) + C(2/5) + C(3/5) + C(4/5) + C(5/5) = 5 + 10 + 10 + 5 = 30
-# B： blur
-# M： blur motion
-# N： noisy
-# L： photo light, 从左到右，越来越白
-# R： photo light, 从右到左，越来越白
-enhance_mark = ['B', 'M', 'N', 'L', 'S']
-# 预先生成所有可能的组合
-all_combinations = []
-for r in range(1, len(enhance_mark) + 1):
-    all_combinations.extend(combinations(enhance_mark, r))
+    # 指定高斯滤波器的内核大小，以控制模糊程度
+    r = random.choice([kernel_min, kernel_max])
+    kernel_size = (r, r)
 
-# 屏蔽B和M同时出现的组合
-all_combinations = [combo for combo in all_combinations if not ('B' in combo and 'M' in combo)]
+    # 应用高斯模糊
+    blurred_image = cv2.GaussianBlur(input_image, kernel_size, 0)
 
-# 屏蔽L和S同时出现的组合
-all_combinations = [combo for combo in all_combinations if not ('L' in combo and 'S' in combo)]
-
-# if len(sys.argv) >= 2:
-#     input_image_path = sys.argv[1]
-
-# if not os.path.exists(input_image_path):
-#     print(f'{input_image_path} does not exist')
-#     exit(-1)
-
-#label_path = os.path.join(input_image_path, 'labels')
-
-# if len(sys.argv) >= 3:
-#     enhance_mark = sys.argv[2]
-
-# for c in enhance_mark:
-#     if c not in ['B', 'M', 'N', 'L', 'S']:
-#         print(f'{enhance_mark} must be either B,M,N,L,S or their combo')
-#         exit(-1)
-
-def process_image(file):
-    time_start = time.time()
-    input_image_path = '/root/autodl-tmp/training_data_gray/'
-    label_path = os.path.join(input_image_path, 'labels')
-    # 随机选择图像增强类型
-    output_image_path = './images_enhanced/'
-    print(file)
-    random.seed(os.getpid() + int(time.time()))
-
-    enhance_mark = "".join(random.choice(all_combinations))
-    print(enhance_mark)
+    return blurred_image
 
 
+# # 模拟生成运动模糊图像
+# def generate_blur_motion(input_image, kernel_min=3, kernel_max=5):
+#     if kernel_min < kernel_max:
+#         kernel_min = kernel_max
+#     # 创建运动模糊内核
+#     r = random.randint(kernel_min, kernel_max)
+#     if r % 2 == 0:
+#         r += 1
+#     kernel_size = r  # 内核大小，控制模糊程度
 
-    file_name_no_ext = file[:file.rfind('.')]
-    file_name_ext = file[file.rfind('.'):]
+#     angle = random.randint(1, 24) * 15   # 运动方向的角度
+#     motion_blur_kernel = np.zeros((kernel_size, kernel_size))
 
-    label_path_output = os.path.join(output_image_path, 'labels')
+#     # 计算内核的中心点
+#     center = (kernel_size - 1) / 2
 
-    # 拷贝label
-    file_path = os.path.join(label_path, file_name_no_ext + ".json")
-    #print(file_path)
-    file_path2 = os.path.join(label_path_output, file_name_no_ext + '_' + enhance_mark + '.json')
-    try:
-        shutil.copy(file_path, file_path2)
-    except:
-        return
+#     # 计算运动方向的起点和终点
+#     start = int(center - kernel_size / 2)
+#     end = int(center + kernel_size / 2)
 
-    if file_name_ext == '.jpg' or file_name_ext == '.png':
-        file_path_name = os.path.join(input_image_path, file)
-        output_image_filename = os.path.join(output_image_path, file_name_no_ext + "_" + enhance_mark + file_name_ext)
+#     # 在内核中设置运动方向
+#     for i in range(start, end + 1):
+#         x = int(center + np.cos(np.deg2rad(angle)) * (i - center))
+#         y = int(center + np.sin(np.deg2rad(angle)) * (i - center))
+#         motion_blur_kernel[y, x] = 1
 
-        im = cv2.imread(file_path_name)
-        is_scan_image = check_image_type(im)
-        if 'B' in enhance_mark:
-            if is_scan_image:
-                im = generate_blur(im, 3, 7)
+#     # 归一化内核，确保内核的总和为1
+#     motion_blur_kernel = motion_blur_kernel / kernel_size
+
+#     # 应用运动模糊
+#     blurred_image = cv2.filter2D(input_image, -1, motion_blur_kernel)
+
+#     return blurred_image
+def generate_blur_motion(input_image, kernel_min, kernel_max):
+    if kernel_min < kernel_max:
+        kernel_min = kernel_max
+
+    # 随机选择内核大小
+    kernel_size = np.random.choice(np.arange(kernel_min, kernel_max + 1, 2))
+    
+    # 随机选择运动方向的角度
+    angle = np.random.randint(1, 25) * 15
+    
+    # 创建运动模糊内核
+    motion_blur_kernel = np.zeros((kernel_size, kernel_size))
+    center = (kernel_size - 1) / 2
+    indices = np.arange(kernel_size)
+    
+    # 计算运动方向的坐标
+    x = np.round(center + np.cos(np.deg2rad(angle)) * (indices - center)).astype(int)
+    y = np.round(center + np.sin(np.deg2rad(angle)) * (indices - center)).astype(int)
+    
+    # 设置运动方向
+    motion_blur_kernel[y, x] = 1
+    
+    # 归一化内核
+    motion_blur_kernel /= kernel_size
+
+    # 应用运动模糊
+    blurred_image = cv2.filter2D(input_image, -1, motion_blur_kernel)
+
+    return blurred_image
+
+# 模拟叠加随机噪点
+
+def gaussian_noise(image, sigma=20):
+    row, col, ch = image.shape
+    mean = 0
+    #sigma = 15
+    # if not degree:
+    #     var = np.random.uniform(0.004, 0.01)
+    # else:
+    #     var = degree
+    # sigma = var ** 0.5 * 25
+    #gauss = np.random.normal(mean, sigma, (row, col, ch))
+    gauss = np.random.normal(mean, sigma, (row, col, 1))
+
+    #gauss = gauss.reshape(row, col, ch)
+    noisy = image + gauss
+    noisy = np.clip(noisy,a_min=0,a_max=255)
+    #noisy = np.array(noisy, dtype=np.uint8)
+    
+    return noisy
+
+
+
+def generate_noisy(img, noise_level=0.01, max_noise_size=2):
+    noisy_img = img.copy()
+
+    # Calculate the total number of noise pixels
+    total_noise_pixels = int(noise_level * img.size / img.shape[2])
+
+    # Counter for number of noise pixels added
+    noise_pixels_added = 0
+
+    while noise_pixels_added < total_noise_pixels:
+        # Randomly pick a pixel
+        x = np.random.randint(0, img.shape[1])
+        y = np.random.randint(0, img.shape[0])
+
+        # Randomly choose the size of the noise (1x1, 2x2, 3x3)
+        size = np.random.randint(1, max_noise_size + 1)
+
+        # Ensure the noise block stays within image bounds
+        if y + size > img.shape[0]:
+            size = img.shape[0] - y
+        if x + size > img.shape[1]:
+            size = img.shape[1] - x
+
+        # Set the chosen area to black
+        noisy_img[y:y + size, x:x + size] = (np.random.randint(0, 80),
+                                             np.random.randint(0, 80),
+                                             np.random.randint(0, 80))
+
+        # Update the counter
+        noise_pixels_added += size * size
+
+    return noisy_img
+
+
+# 模拟手机拍照叠加光照效果
+def generate_photo_light(input_image, direction=-1, darkness=0.7):
+    #direction: -1 random, 0 up, 1 down, 2 left, 3 right
+    #ratio = 4 / 60
+    #print(input_image.shape)
+    if direction == -1:
+        direction = np.random.randint(0, 4)
+
+    #print(direction)
+    #if input_image.shape == 3:
+    #    input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+    #flattened_image = input_image.flatten()
+
+    # 计算排序后的百分位数
+    # low_value = np.percentile(flattened_image, percentile_low)
+    # high_value = np.percentile(flattened_image, percentile_high)
+
+    low_value = np.min(input_image) + 20
+    high_value = np.max(input_image) - 20
+
+    ratio = min((high_value - low_value) * darkness, 80)
+    #ratio = (high_value - low_value) 
+
+    #ratio = min((high_value - low_value) * 0.8, low_value *1.2)
+    ratio = int(ratio) 
+    #print("ratio",low_value/255,(high_value - low_value) /255)
+    #ratio = ratio * 0.35
+    print("ratio",ratio)
+
+    # Create a mask of the same size as the image, filled with horizontal gradient
+    mask = np.zeros_like(input_image, dtype=np.uint8)
+    
+    if direction in [2,3]:
+        max_intensity = ratio #* input_image.shape[1]
+
+        # Calculate gradient
+        for x in range(input_image.shape[1]):
+            intensity = int(x / input_image.shape[1] * max_intensity)
+            if direction == 2:
+                mask[:, x] = max_intensity - intensity
             else:
-                im = generate_blur(im, 3, 5)
-        if 'M' in enhance_mark:
-            if is_scan_image:
-                im = generate_blur_motion(im, 3, 7)
-            else:
-                im = generate_blur_motion(im, 3, 5)
-        if 'N' in enhance_mark:
-            im = generate_noisy(im, noise_level=0.01, max_noise_size=3)
-        if 'L' in enhance_mark:
-            im = generate_photo_light(im)
-        if 'S' in enhance_mark:
-            im = generate_photo_shadow(im)
-        print(output_image_filename)
+                mask[:, x] = intensity
 
-        cv2.imwrite(output_image_filename, im)
-        print("time:",time.time()-time_start)
+        # Apply the light effect mask to the original image
+        light_effect_image = cv2.subtract(input_image, mask,dtype=cv2.CV_8U)
+    
+    elif direction in [0,1]:
+        max_intensity = ratio #* input_image.shape[0]
+
+        # Calculate gradient
+        for x in range(input_image.shape[0]):
+            intensity = int(x / input_image.shape[0] * max_intensity)
+            if direction == 0:
+                mask[x, :] = max_intensity - intensity
+            else:
+                mask[x, :] = intensity
+
+        # Apply the light effect mask to the original image
+        light_effect_image = cv2.subtract(input_image, mask,dtype=cv2.CV_8U)
+
+    return light_effect_image
+
+
+# def generate_photo_shadow(input_image,percentile_low=1,percentile_high=99):
+#         # 将图像转换为一维数组
+
+#     if len(input_image.shape) == 3:
+#         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+#     #flattened_image = input_image.flatten()
+
+#     # 计算排序后的百分位数
+#     # low_value = np.percentile(flattened_image, percentile_low)
+#     # high_value = np.percentile(flattened_image, percentile_high)
+#     low_value = np.min(input_image) +10
+#     high_value= np.max(input_image) -10
+#     #print(low_value,high_value)
+
+#     # 生成多边形的凸包
+#     mask = np.zeros_like(input_image, dtype=np.uint8)
+
+#     num_points = np.random.randint(3, 20)
+#     points = np.random.randint(0, min(input_image.shape[0], input_image.shape[1]), size=(num_points, 2))
+#     hull = cv2.convexHull(points)
+#     # 在初始掩码上填充凸包的区域
+#     cv2.fillPoly(mask, [hull], 255)
+    
+#     # 用灰度值填充凸包区域（在原本像素低于阴影像素时进行填充）
+#     depth = np.random.randint((5*low_value + 5*high_value)/10,(1 * low_value + 9 * high_value)/10)  # 随机生成灰度值
+#     #print(depth)
+#     #depth = (4*low_value + 6*high_value)/10
+
+#     mask_shadow = np.ones_like(input_image, dtype=np.uint8)
+#     mask_shadow = mask_shadow  *  np.random.uniform(0.3, 0.7) * (high_value - low_value)
+#     # 剪切原图中的阴影区域
+#     shadow_cut_image =  np.where(mask > 0, input_image, 0)
+#     mask_shadow = np.where(mask > 0, mask_shadow, 0)
+#     #shadow_cut_image =  cv2.subtract(shadow_cut_image, mask_shadow)
+#     shadow_cut_image = cv2.subtract(shadow_cut_image.astype(np.float32), mask_shadow.astype(np.float32), dtype=cv2.CV_8U)
+
+#     #np.where(shadow_cut_image > depth, depth , shadow_cut_image)
+
+#     shadow_effect_image = np.where(mask > 0, shadow_cut_image, input_image)
+#     return shadow_effect_image
+
+
+def generate_photo_shadow(input_image, darkness=0.7):
+    # 将图像转换为一维数组
+    #if input_image.shape[-1] == 3:
+    #    input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+    # 计算排序后的百分位数
+    low_value = np.min(input_image) + 20
+    high_value = np.max(input_image) - 20
+
+    # 生成多边形的凸包
+    mask = np.zeros_like(input_image, dtype=np.uint8)
+
+    num_points = np.random.randint(3, 20)
+    points = np.random.randint(0, min(input_image.shape[0], input_image.shape[1]), size=(num_points, 2))
+    hull = cv2.convexHull(points)
+    # 在初始掩码上填充凸包的区域
+    cv2.fillPoly(mask, [hull], 255)
+
+    # 用灰度值填充凸包区域（在原本像素低于阴影像素时进行填充）
+    depth = np.random.randint((5 * low_value + 5 * high_value) / 10, (1 * low_value + 9 * high_value) / 10)
+
+    mask_shadow = darkness * (high_value - low_value)
+    mask_shadow = (mask_shadow * np.ones_like(input_image)).astype(np.uint8)
+
+    # 剪切原图中的阴影区域
+    shadow_cut_image = np.where(mask > 0, input_image, 0)
+    mask_indices = mask > 0
+    shadow_cut_image = cv2.subtract(shadow_cut_image.astype(np.float32), mask_shadow.astype(np.float32), dtype=cv2.CV_8U)
+
+    # np.where(shadow_cut_image > depth, depth, shadow_cut_image)
+    shadow_cut_image = np.clip(shadow_cut_image, None, depth)
+
+    shadow_effect_image = np.where(mask_indices, shadow_cut_image, input_image)
+    return shadow_effect_image
+
+
+
+def check_image_type(image):
+    # 确保图像是灰度图
+    if len(image.shape) > 2:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = image
+
+    # 获取图像的宽度和高度
+    h, w = gray.shape
+    gray = gray[int(0.2*h):int(0.8*h), int(0.2*w):int(0.8*w)]
+    h, w = gray.shape
+
+    # 随机选择100个像素点
+    random_pixels = [gray[np.random.randint(h), np.random.randint(w)] for _ in range(100)]
+
+    # 找到这100个点中的最大值
+    max_value = max(random_pixels)
+
+    # 判断图像类型
+    if max_value > 235:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
-    output_image_path = './images_enhanced/'
-    input_image_path = '/root/autodl-tmp/training_data_gray/'
+    #file_path_name = './tools/annotation/test3.jpg'
+    file_path_name = '/root/autodl-tmp/Code/image_tools-master/test3.jpg'
 
-    items = os.listdir(input_image_path)
-    # 过滤出子目录
-    image_files = [entry for entry in items if os.path.isfile(os.path.join(input_image_path, entry))]
 
-    print(len(image_files))
-    #output_files =[os.path.join(output_image_path, entry) for entry in items if os.path.isfile(os.path.join(input_image_path, entry))]
+    file_path = file_path_name[:file_path_name.rfind('/')]
+    file_name = file_path_name[file_path_name.rfind('/') + 1:]
+    file_name_no_ext = file_name[:file_name.rfind('.')]
+    print(file_path_name)
+    im = cv2.imread(file_path_name)
+
+
+    hello = check_image_type(im)
+    time_start = time.time()
+    im1 = generate_blur(im, 3, 7)
+    cv2.imwrite(os.path.join(file_path, file_name_no_ext + '_1.jpg'), im1)
     
-    if not os.path.exists(output_image_path):
-        os.makedirs(output_image_path)
+    print(time.time()-time_start)
+    time_start = time.time()
+
+    im2 = generate_blur_motion(im, 3, 9)
+    cv2.imwrite(os.path.join(file_path, file_name_no_ext + '_2.jpg'), im2)
+
+    print("time",time.time()-time_start)
+    time_start = time.time()
+
+    im3 = generate_noisy(im)
+    cv2.imwrite(os.path.join(file_path, file_name_no_ext + '_3.jpg'), im3)
+
+    print("time",time.time()-time_start)
+
+    time_start = time.time()
+
+    im4 = generate_photo_light(im,direction=1,darkness=0.3)
+    cv2.imwrite(os.path.join(file_path, file_name_no_ext + '_4.jpg'), im4)
+
+    print("time",time.time()-time_start)
+    time_start = time.time()
+
+    im5 = generate_photo_light(im,direction=3,darkness=0.3)
+    cv2.imwrite(os.path.join(file_path, file_name_no_ext + '_5.jpg'), im5)
+
+    print("time",time.time()-time_start)
+    time_start = time.time()
+
+    im6 = generate_photo_shadow(im)
+    cv2.imwrite(os.path.join(file_path, file_name_no_ext + '_6.jpg'), im6)
+
+    print("time",time.time()-time_start)
+    time_start = time.time()
     
-    label_path_output = os.path.join(output_image_path, 'labels')
-    if not os.path.exists(label_path_output):
-        os.makedirs(label_path_output)
+    im7 =gaussian_noise(im)
+    cv2.imwrite(os.path.join(file_path, file_name_no_ext + '_7.jpg'), im7)
 
-
-    with ProcessPoolExecutor(max_workers=12) as executor:
-        # Get the current process ID
-        current_process_id = os.getpid()
-        
-        # Set affinity for the current process (optional)
-        set_affinity(current_process_id, 0)  # You can choose a specific CPU core
-        
-        # Use executor.submit to set affinity for each submitted task
-        futures = [executor.submit(set_affinity, os.getpid(), i % psutil.cpu_count()) for i in range(12)]
-
-        # Process the images
-        for file in image_files:
-            executor.submit(process_image, file).result()
+    print("time",time.time()-time_start)
